9c3f245a0890ef35d1267a96e161bd2c8c606a5b
Mango <greatdogsite@gmail.com>
2023-03-13 19:38:06 -0500

    studio complete

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,14 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true }
   ];
+
+  memberBeingEdited: object = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  add(name, checked) {
+    let newObject = { name: name, firstMission: checked };
+    this.crew.push(newObject);
+  }
+  remove(person) {
+    let index = this.crew.indexOf(person);
+    this.crew.splice(index, 1);
+  }
+  edit(person) {
+    this.memberBeingEdited = person;
+    console.log(this.memberBeingEdited);
+  }
+  nameChange(person, newName) {
+    // let index = this.crew.indexOf(this.memberBeingEdited)
+    // this.crew[index]["name"] = newName;
+    console.log(person);
+    console.log(newName);
+    person["name"] = newName;
+    this.memberBeingEdited = null;
   }
 
 }
